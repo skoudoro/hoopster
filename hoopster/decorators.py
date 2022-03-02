c8fed077f874f83ab11e10938c11fd1188484679
Serge Koudoro <skab12@gmail.com>
2022-03-02 05:02:09 +0100

    manage generic alias

--- hoopster/decorators.py
+++ hoopster/decorators.py
@@ -1,12 +1,12 @@
 
 from dataclasses import dataclass, is_dataclass
+from typing import get_args
 
 
 # decorator to wrap original __init__
 def nested_dataclass(*args, **kwargs):
 
     def wrapper(check_class):
-
         # passing class to investigate
         check_class = dataclass(check_class, **kwargs)
         o_init = check_class.__init__
@@ -20,9 +20,16 @@ def nested_dataclass(*args, **kwargs):
 
                 if is_dataclass(ft) and isinstance(value, dict):
                     obj = ft(**value)
-                    kwargs[name]= obj
+                    kwargs[name] = obj
+                # manage genericalias
+                elif '__origin__' in dir(ft):
+                    if ft.__origin__ == list:
+                        sub_ft = get_args(ft)[0]
+                        if is_dataclass(sub_ft):
+                            kwargs[name] = [sub_ft(**v) for v in value]
+
                 o_init(self, *args, **kwargs)
-        check_class.__init__=__init__
+        check_class.__init__ = __init__
 
         return check_class
 
